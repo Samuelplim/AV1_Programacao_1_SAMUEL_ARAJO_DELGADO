3a2b4c84dea39d28978d9c9e316f5c7c6db290cd
Samuelplim <33794046+Samuelplim@users.noreply.github.com>
2020-05-13 21:48:17 -0400

    Questão 4

--- src/view/Main.java
+++ src/view/Main.java
@@ -7,12 +7,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Conta contaTeste = new Conta("Samuel Delgado","Santander",5578888);
+        Conta contaTeste1 = new Conta("Spotify-ID:0123555","Itau",135578);
         contaTeste.depositar(2222);
+        contaTeste1.depositar(100000);
         contaTeste.sacar(1000);
 
         System.out.println(contaTeste.toString());
 
-        Conta contaTeste1 = new Conta("Spotify-ID:0123555","Itau",135578);
+
         contaTeste.trasferir(contaTeste1,16.50);
 
         System.out.println(contaTeste.toString());
