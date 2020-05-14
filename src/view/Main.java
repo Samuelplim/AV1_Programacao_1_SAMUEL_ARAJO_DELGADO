8877af4dbbbe9b8756bda5020204fd64b5d9946e
Samuelplim <33794046+Samuelplim@users.noreply.github.com>
2020-05-13 23:23:52 -0400

    Questão 6
    Não é possivel executar pois a Classe "Conta" é abstrada, e classes abstradas não podem ser instanciadas

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,15 +1,24 @@
 package view;
 
 import entities.Conta;
+import entities.ContaCorrente;
+import entities.ContaPoupanca;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-
+        /*
+        Não é possivel executar pois a Classe "Conta" é abstrada, e classes abstradas não podem ser instanciadas
         Conta contaTeste = new Conta();
         Conta contaTeste1 = new Conta();
+        */
+        ContaPoupanca contaTeste = new ContaPoupanca("Samuel Delgado", "Banco do Brasil", 878,
+                3.6,28);
+        
+        ContaCorrente contaTeste1 = new ContaCorrente("Spotify-598775-99","Santander",5789,
+                15,50000);
+
         contaTeste.depositar(2222);
         contaTeste1.depositar(100000);
         contaTeste.sacar(1000);
